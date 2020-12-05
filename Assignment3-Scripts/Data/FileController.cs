90ccaf988abf837836d48e7d76f5fcb1d1677f8c
ChristyWithCoding <72668464+ChristyWithCoding@users.noreply.github.com>
2020-12-05 22:03:29 +0000

    Add Data File Control Scripts
    Initialize the "FileController.cs". "FileController.cs" is used to control the actual data file in the game by associating it with the Game Object in the game. FileController.cs calls the methods of the CSV class, creates a CSV instance, imports the datafile, and reads the datafile line by line.

---
+++ Assignment3-Scripts/Data/FileController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CSV.GetInstance().loadFile(Application.dataPath + "/Res", "myTest.csv");
+        for (int i = 0; i < CSV.GetInstance().m_ArrayData.Count; i++)
+        {
+            Debug.Log(CSV.GetInstance().m_ArrayData[i]);
+        }
+    }
+}
