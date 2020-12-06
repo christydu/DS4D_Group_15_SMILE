b65b5e6cf85c37524fcc848a383933a4c07ec768
Xiaohan Hu <32345943+ariiyeolll@users.noreply.github.com>
2020-12-06 12:51:30 +0000

    Add files via upload
    Add "GraphDisplay" script to display and close graphs for each region.

---
+++ Assignment3-Scripts/UI/GraphDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphDisplay: MonoBehaviour {
+
+ // Use this for initialization
+    public GameObject buttonShow;
+    public GameObject buttonClose;
+    public GameObject Scene;
+    public GameObject Sceneclose;
+    public GameObject Scenecontinue;
+    private void Start()
+     {
+         buttonShow.GetComponent<Button>().onClick.AddListener(Show);
+         buttonClose.GetComponent<Button>().onClick.AddListener(Close);
+         Sceneclose.GetComponent<Button>().onClick.AddListener(Scenebuttonclose);
+         Scenecontinue.GetComponent<Button>().onClick.AddListener(Show);
+
+     }
+     void Show()
+     {
+        Scene.GetComponent<Image>().enabled = true;
+        Sceneclose.GetComponent<Image>().enabled = true;
+        Scenecontinue.GetComponent<Image>().enabled = true;
+    }
+    void Close()
+     {
+        Scene.GetComponent<Image>().enabled = false;
+    }
+    void Scenebuttonclose()
+    {
+        Sceneclose.GetComponent<Image>().enabled = false;
+        Scenecontinue.GetComponent<Image>().enabled = false;
+    }
+ }
