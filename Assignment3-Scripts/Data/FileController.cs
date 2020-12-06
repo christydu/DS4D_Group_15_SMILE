158fa3e409fc3cff3fbb5af853e23b64be73bc73
ChristyWithCoding <72668464+ChristyWithCoding@users.noreply.github.com>
2020-12-06 12:03:12 +0000

    Define and assign values to game data variables
    Define nested list variables in the same way that data is used in the game. https://github.com/ChristyWithCoding/DS4D_Group_15_SMILE/blob/a3605721d36c6d61edb18a17115955a2f73c8e44/Assignment3-Scripts/MainControllerExamples/MainGameController.cs#L149
    
    Each of these elements is a Customer list, which stores information about three attributes of the Customer.
    
    In the basic method of the FileController Class, the data read from the CSV instance is assigned to the Customer List.

--- Assignment3-Scripts/Data/FileController.cs
+++ Assignment3-Scripts/Data/FileController.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class CustomerList: List<Customer> {}
+
+    public class Customer: List<string> {
+      public string Type {get; set;}
+      public string Time {get; set;}
+      public string ServiceName {get; set;}
+    }
+
+    static public CustomerList customer_list;
+
+
 public class FileController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        CSV.GetInstance().loadFile(Application.dataPath + "/Res", "myTest.csv");
-        for (int i = 0; i < CSV.GetInstance().m_ArrayData.Count; i++)
+        CustomerList customer_list = new CustomerList();
+        CSV.GetInstance().loadFile(Application.dataPath + "/Fruit-Juice-Maker/Res", "testcsv.csv");
+        for (int i = 1; i < CSV.GetInstance().m_ArrayData.Count; i++)
         {
-            Debug.Log("getString: " + CSV.GetInstance().getString(1,1));
-            Debug.Log("getInt: " + CSV.GetInstance().getInt(1,2));
+            Customer one_customer = new Customer();
+            one_customer.Type = CSV.GetInstance().getString(i,0);
+            one_customer.Time = CSV.GetInstance().getString(i,1);
+            one_customer.ServiceName = CSV.GetInstance().getString(i,2);
         }
     }
 }
