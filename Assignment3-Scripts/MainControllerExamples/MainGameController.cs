64dcb47e1615bf87b8611f6c06678c9b91a7166d
Xiaohan Hu <32345943+ariiyeolll@users.noreply.github.com>
2020-12-06 22:24:13 +0000

    Add "using UnityEngine.UI;"

--- Assignment3-Scripts/MainControllerExamples/MainGameController.cs
+++ Assignment3-Scripts/MainControllerExamples/MainGameController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Linq;
 using System.Collections.Generic;
 using System.Threading;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainGameController : MonoBehaviour
