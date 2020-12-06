76b5a7abc4ddd7ce78941e51cf872b37429cc77b
Xiaohan Hu <32345943+ariiyeolll@users.noreply.github.com>
2020-12-06 13:29:42 +0000

    Add "ToMenu" file
    Switch from "RegionSelection" scene to main menu scene "Menu-c#".

---
+++ Assignment3-Scripts/UI/ToMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ToMenu : MonoBehaviour {
+	
+	// Use this for initialization
+	public GameObject Backbutton_R;
+
+	void Start () {
+		Backbutton_R.GetComponent<Button>().onClick.AddListener(OnClick1);
+    }
+
+    void OnClick1()
+    {
+        SceneManager.LoadScene("Menu-c#"); //back to main menu scene
+    }
+
+}
